7e644ff8f5f225df2505f431a06539d6b344a1e1
InnaGGB <sh-noki@yandex.ru>
2023-04-29 17:55:07 +0300

    HW_3_ifelse_switch

---
+++ HW_3_1.js
@@ -0,0 +1,24 @@
+let numOne = +prompt ('Enter the 1st value');
+let numTwo = +prompt ('Enter the 2nd value');
+let operator = prompt ('type the operator');
+switch (operator) {
+    case ('/'):
+    numOne !=0;
+    numTwo !=0;
+    console.log (numOne/numTwo);
+    break;
+    case ('/'):
+    numOne = 0;
+    numTwo = 0;
+    console.log ("You can't divide by 0");
+    break;
+    case ('+'):
+    console.log ((numOne) + (numTwo));
+    break;
+    case ('-'):
+    console.log (numOne - numTwo);
+    break;
+    case ('*'):
+    console.log (numOne * numTwo);
+    break;
+}
